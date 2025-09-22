6ffb329924bb36e6cd286f3320a7bdd140cdef36
papawin1st <papawin1st@gmail.com>
2025-09-22 14:26:49 +0700

    add test send back

---
+++ scenario/test_submit_sendback.js
@@ -0,0 +1,116 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+  vus: 6,
+  iterations: 6,
+  // duration: "1m",
+  //  thresholds: {
+  //   http_req_failed: ['rate<0.01'], // fail test ถ้า error > 1%
+  // },
+};
+const header = JSON.parse(open("../data/header/header.json"));
+const header1 = header["user1"];
+const header2 = header["user2"];
+const header3 = header["user3"];
+const header4 = header["user4"];
+const header5 = header["user5"];
+const header6 = header["user6"];
+const headerAdmin = header["admin"];
+const headers = [header1, header2, header3, header4, header5, header6];
+const bodySchema1 = JSON.parse(open("../data/body/submit_step1_body.json"));
+const bodySchema2 = JSON.parse(open("../data/body/submit_step2_body.json"));
+const bodySchema3 = JSON.parse(open("../data/body/submit_step3_body.json"));
+const bodySchema4 = JSON.parse(open("../data/body/submit_step4_body.json"));
+const bodySchema5 = JSON.parse(open("../data/body/submit_step5_body.json"));
+const bodySchema6 = JSON.parse(open("../data/body/submit_step6_body.json"));
+const bodySchema = [
+  bodySchema1,
+  bodySchema2,
+  bodySchema3,
+  bodySchema4,
+  bodySchema5,
+  bodySchema6,
+];
+const pmFormIDs = ["94624", "94625", "94626", "94627", "94628", "94629"];
+const stepNumbers = [1, 2, 3, 4, 5, 6];
+
+export default function () {
+  // Define
+  const userIndex = (__VU - 1) % options.vus;
+  const baseUrl = "https://pmsapiuat.thaibev.com";
+  const url = `${baseUrl}/performance/submit-pm-form`;
+  const params = { headers: headers[userIndex] };
+  const body = JSON.stringify(bodySchema[userIndex]);
+
+  // restore
+  if (stepNumbers[userIndex] > 4) {
+    restorePerformance(baseUrl, headers[userIndex], userIndex);
+  }
+
+  // Load Test
+  const response = http.post(url, body, params);
+  const bodyResponse = response.json();
+  console.log(pmFormIDs[userIndex], stepNumbers[userIndex]);
+  // Validate
+  const status = check(response, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  if (status == false) {
+    const bodyString = JSON.stringify(bodyResponse);
+    console.log(`Step${userIndex + 1}: ${bodyString}`);
+  } else {
+    // sendback;
+    if (stepNumbers[userIndex] <= 4) {
+      sendBackForm(
+        baseUrl,
+        Number(pmFormIDs[userIndex]),
+        stepNumbers[userIndex]
+      );
+    }
+  }
+}
+
+function restorePerformance(baseUrl, header, userIndex) {
+  const url = `${baseUrl}/admin/restore-performance?pmFormID=${pmFormIDs[userIndex]}&stepNumber=${stepNumbers[userIndex]}`;
+  const params = { headers: header };
+  const response = http.post(url, null, params);
+  // Validate
+  const bodyResponse = response.json();
+  const status = check(response, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  if (status == false) {
+    const bodyString = JSON.stringify(bodyResponse);
+    console.log(
+      `restore-performance, pm form id ${pmFormIDs[userIndex]} step number ${stepNumbers[userIndex]}: ${bodyString}`
+    );
+  }
+}
+
+function sendBackForm(baseUrl, pmFormID, stepNumber) {
+  const bodySendBack = {
+    pmFormID: pmFormID,
+    currentStep: stepNumber + 1,
+    nextStep: stepNumber,
+    isSendEmail: false,
+  };
+  const url = `${baseUrl}/admin/route-form`;
+  const params = { headers: headerAdmin };
+  const response = http.post(url, JSON.stringify(bodySendBack), params);
+  // Validate
+  const bodyResponse = response.json();
+  const status = check(response, {
+    "status is 200": (r) => r.status === 200,
+  });
+  if (status == false) {
+    const bodyString = JSON.stringify(bodyResponse);
+    console.log(
+      `send-back-performance, pm form id ${pmFormID} step number ${stepNumber}: ${bodyString} ,"bodySendBack": ${JSON.stringify(
+        bodySendBack
+      )}`
+    );
+  }
+}
